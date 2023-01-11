fece901b2b57cd4ae60d77b706189f215e9f9ea2
Anthony ZORZETTO <zorzetto.anthony@gmail.com>
2023-01-11 20:18:08 +0100

    Add delete

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -85,7 +85,25 @@ export class PhotoService {
       }
     }
   }
-
+  public async deletePicture(photo: UserPhoto, position: number) {
+    // Remove this photo from the Photos reference data array
+    this.photos.splice(position, 1);
+  
+    // Update photos array cache by overwriting the existing photo array
+    Preferences.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos)
+    });
+  
+    // delete photo file from filesystem
+    const filename = photo.filepath
+                        .substr(photo.filepath.lastIndexOf('/') + 1);
+  
+    await Filesystem.deleteFile({
+      path: filename,
+      directory: Directory.Data
+    });
+  }
 
 
   private async readAsBase64(photo: Photo) {
@@ -115,6 +133,7 @@ export class PhotoService {
     };
     reader.readAsDataURL(blob);
   });
+
 }
 
 export interface UserPhoto {
